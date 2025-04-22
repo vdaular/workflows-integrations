6f579d5e2bfeb6690da6b6093b30e5a1b81bc1c6
Matt <knibbsy10@live.com>
2025-04-22 01:06:33 +0100

    Update projects & namespaces to now include Integrations within them.

---
+++ src/sql/Elsa.Integrations.Sql.PostgreSql/PostgreSqlClient.cs
@@ -0,0 +1,16 @@
+﻿using Npgsql;
+using Elsa.Integrations.Sql.Client;
+using System.Data.Common;
+
+namespace Elsa.Integrations.Sql.PostgreSql;
+
+/// <summary>
+/// PostgreSQL client implementation.
+/// </summary>
+/// <param name="connectionString"></param>
+public class PostgreSqlClient(string connectionString) : BaseSqlClient(connectionString)
+{
+    protected override DbConnection CreateConnection() => new NpgsqlConnection(_connectionString);
+
+    protected override DbCommand CreateCommand(string query, DbConnection connection) => new NpgsqlCommand(query, (NpgsqlConnection)connection);
+}
---
+++ src/sql/Elsa.Integrations.Sql.SqlServer/SqlServerClient.cs
@@ -0,0 +1,16 @@
+﻿using System.Data.Common;
+using Elsa.Integrations.Sql.Client;
+using Microsoft.Data.SqlClient;
+
+namespace Elsa.Integrations.Sql.SqlServer;
+
+/// <summary>
+/// Microsoft SQL server client implementation.
+/// </summary>
+/// <param name="connectionString"></param>
+public class SqlServerClient(string connectionString) : BaseSqlClient(connectionString)
+{
+    protected override DbConnection CreateConnection() => new SqlConnection(_connectionString);
+
+    protected override DbCommand CreateCommand(string query, DbConnection connection) => new SqlCommand(query, (SqlConnection)connection);
+}
