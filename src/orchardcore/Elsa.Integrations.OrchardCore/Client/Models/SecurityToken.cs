9204661fbe7d77b4cf147d69eb30aa4029a4733a
Matt <knibbsy10@live.com>
2025-05-16 00:30:48 +0100

    Remove Integration from Azure projects and split them into new folders.

--- src/orchardcore/Elsa.Integrations.OrchardCore/Client/Models/SecurityToken.cs
+++
@@ -1,7 +0,0 @@
-namespace Elsa.Integrations.OrchardCore.Client.Models;
-
-/// <summary>
-/// Represents a security token used for authentication, including access token,
-/// token type, and expiration information.
-/// </summary>
-public record SecurityToken(string AccessToken, string TokenType, int ExpiresIn);
---
+++ src/storage/Elsa.AzureStorage/Activities/UploadBlobActivity.cs
@@ -0,0 +1,50 @@
+﻿using Azure.Storage.Blobs.Specialized;
+using Elsa.AzureStorage.Extensions;
+using Elsa.AzureStorage.Services;
+using Elsa.Workflows;
+using Elsa.Workflows.Attributes;
+using Elsa.Workflows.Models;
+using JetBrains.Annotations;
+
+namespace Elsa.AzureStorage.Activities;
+
+/// <summary>
+/// Represents an activity used to upload a blob to Azure Storage.
+/// </summary>
+[Activity(
+    "Elsa.Integrations.Storage",
+    "Storage",
+    "Uploads a blob to Azure Storage.",
+    DisplayName = "Upload blob", Kind = ActivityKind.Task)]
+[UsedImplicitly]
+public class UploadBlob : Activity
+{
+    /// <summary>
+    /// The content to upload.
+    /// </summary>
+    [Input(Description = "The content to upload.")]
+    public Input<object> Content { get; set; } = null!;
+    
+    // TODO: Replace this with the new Connection API that is coming soon.
+    /// <summary>
+    /// The connection string for the Azure Storage account.
+    /// </summary>
+    public Input<string> ConnectionString { get; set; } = null!;
+
+    /// <summary>
+    /// The name of the blob to upload.
+    /// </summary>
+    public Input<string> BlobName { get; set; } = null!;
+
+    /// <inheritdoc />
+    protected override async ValueTask ExecuteAsync(ActivityExecutionContext context)
+    {
+        var cancellationToken = context.CancellationToken;
+        var connectionString = context.Get(ConnectionString)!;
+        var blobName = context.Get(BlobName)!;
+        var content = context.Get(Content)!;
+        var blobContainerClientFactory = context.GetRequiredService<BlobContainerClientFactory>();
+        var blockBlobClient = blobContainerClientFactory.GetBlobContainerClient(connectionString).GetBlockBlobClient($"{blobName}.json");
+        await blockBlobClient.UploadJsonAsBlocksAsync(content, cancellationToken: cancellationToken);
+    }
+}
