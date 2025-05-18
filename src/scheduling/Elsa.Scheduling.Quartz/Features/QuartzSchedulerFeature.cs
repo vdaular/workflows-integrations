c999d524cb22d28a79e68f113e0f06d936b57ab1
Matt <knibbsy10@live.com>
2025-05-18 20:40:39 +0100

    Import Scheduling projects into Extensions.

---
+++ src/scheduling/Elsa.Scheduling.Quartz/Features/QuartzSchedulerFeature.cs
@@ -0,0 +1,47 @@
+using Elsa.Extensions;
+using Elsa.Features.Abstractions;
+using Elsa.Features.Attributes;
+using Elsa.Features.Services;
+using Elsa.Scheduling.Quartz.Contracts;
+using Elsa.Scheduling.Quartz.Handlers;
+using Elsa.Scheduling.Quartz.Services;
+using Elsa.Scheduling.Quartz.Tasks;
+using Elsa.Scheduling;
+using Elsa.Scheduling.Features;
+using Elsa.Workflows;
+using Microsoft.Extensions.DependencyInjection;
+using Quartz;
+
+namespace Elsa.Scheduling.Quartz.Features;
+
+/// <summary>
+/// A feature that installs Quartz.NET implementations for <see cref="IWorkflowScheduler"/>.
+/// </summary>
+[DependsOn(typeof(SchedulingFeature))]
+public class QuartzSchedulerFeature(IModule module) : FeatureBase(module)
+{
+    /// <inheritdoc />
+    public override void Configure()
+    {
+        Module.Configure<SchedulingFeature>(scheduling =>
+        {
+            // Configure the scheduling feature to use the Quartz workflow scheduler.
+            scheduling.WorkflowScheduler = sp => sp.GetRequiredService<QuartzWorkflowScheduler>();
+
+            // Configure the cron parser to use the Quartz cron parser.
+            scheduling.CronParser = sp => sp.GetRequiredService<QuartzCronParser>();
+        });
+    }
+
+    /// <inheritdoc />
+    public override void Apply()
+    {
+        Services
+            .AddSingleton<IActivityDescriptorModifier, CronActivityDescriptorModifier>()
+            .AddSingleton<QuartzCronParser>()
+            .AddScoped<QuartzWorkflowScheduler>()
+            .AddScoped<IJobKeyProvider, JobKeyProvider>()
+            .AddStartupTask<RegisterJobsTask>()
+            .AddQuartz();
+    }
+}
